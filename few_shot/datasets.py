92120b96eb7118164b9a98509f09b003ce6dc6f3
KamalMustafa <kamal.mustafa.ks@gmail.com>
2020-08-15 01:51:57 +0200

    [fix]: Erronous cropping after resize

--- few_shot/datasets.py
+++ few_shot/datasets.py
@@ -235,7 +235,6 @@ class FashionDataset(Dataset):
             self.transform = transforms.Compose([
                 transforms.CenterCrop(c),
                 transforms.Resize((r,r)),
-                transforms.CenterCrop(c),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])

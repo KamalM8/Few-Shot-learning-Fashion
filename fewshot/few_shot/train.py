6995cfac189ba153298ef31293caead1c4f2c0ac
KamalMustafa <kamal.mustafa.ks@gmail.com>
2020-08-14 02:59:26 +0200

    [fix]: Visualization clipping fix

--- fewshot/few_shot/train.py
+++ fewshot/few_shot/train.py
@@ -3,6 +3,8 @@ The `fit` function in this file implements a slightly modified version
 of the Keras `model.fit()` API.
 """
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim import Optimizer
 from torch.nn import Module
@@ -52,6 +54,35 @@ def batch_metrics(model: Module, y_pred: torch.Tensor, y: torch.Tensor, metrics:
             batch_logs = m(y, y_pred)
 
     return batch_logs
+
+def unnormalize(imgs, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    unnorm_imgs = torch.empty_like(imgs)
+    mean_tensor = torch.FloatTensor(mean).view(3,1,1)
+    std_tensor = torch.FloatTensor(std).view(3,1,1)
+    for i, img in enumerate(imgs):
+        unnorm_imgs[i] = img*std_tensor + mean_tensor
+    return unnorm_imgs
+
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    npimg = img.numpy()
+    npimg = (npimg*255).astype(np.uint8)
+    if one_channel:
+        plt.imshow(np.img, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1,2,0)))
+
+def plot_classes_preds(images, labels, preds, n_shot, k_way):
+    support = images[:n_shot*k_way].cpu()
+    support = unnormalize(support)
+    probs = preds
+    fig = plt.figure(figsize=(10,10))
+    for idx in np.arange(support.shape[0]):
+        ax = fig.add_subplot(k_way, n_shot, idx+1, xticks=[], yticks=[])
+        matplotlib_imshow(support[idx], one_channel=False)
+
+    return fig
 
 
 def fit(model: Module, optimiser: Optimizer, loss_fn: Callable, epochs: int, dataloader: DataLoader, writer: SummaryWriter,
@@ -119,8 +150,15 @@ def fit(model: Module, optimiser: Optimizer, loss_fn: Callable, epochs: int, dat
             fit_function_kwargs['stnoptim'] = stnoptim
             fit_function_kwargs['args'] = args
 
-            loss, y_pred = fit_function(model, optimiser, loss_fn, x, y, **fit_function_kwargs)
+            n_shot = fit_function_kwargs['n_shot']
+            k_way = fit_function_kwargs['k_way']
+
+            loss, y_pred, aug_imgs = fit_function(model, optimiser, loss_fn, x, y, **fit_function_kwargs)
             batch_logs['loss'] = loss.item()
+
+            if batch_index % 100 == 99:
+                writer.add_figure('episode', plot_classes_preds(aug_imgs, y, y_pred, n_shot, k_way),
+                        global_step=len(dataloader)*(epoch-1) + batch_index)
 
             # Loops through all metrics
             batch_logs = batch_metrics(model, y_pred, y, metrics, batch_logs)

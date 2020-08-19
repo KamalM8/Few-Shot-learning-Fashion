d4dd09084c7433343a33aa7bf1aa8a2fe381c2c1
KamalMustafa <kamal.mustafa.ks@gmail.com>
2020-08-19 21:25:12 +0200

    [fix]: modified experiments to include stns

--- proto_experiments.sh
+++ proto_experiments.sh
@@ -13,7 +13,14 @@ python experiments/proto_nets.py --dataset fashion --k-test 15 --n-test 5 --n-tr
 python experiments/proto_nets.py --dataset fashion --k-test 2 --n-test 1 --n-train 1 --k-train 20 --augment --size large
 python experiments/proto_nets.py --dataset fashion --k-test 5 --n-test 1 --n-train 1 --k-train 20 --augment --size large
 python experiments/proto_nets.py --dataset fashion --k-test 15 --n-test 1 --n-train 1 --k-train 20 --augment --size large
-python experiments/proto_nets.py --dataset fashion --k-test 2 --n-test 5 --n-train 5 --k-train 20 --augment --size large
-python experiments/proto_nets.py --dataset fashion --k-test 5 --n-test 5 --n-train 5 --k-train 20 --augment --size large
-python experiments/proto_nets.py --dataset fashion --k-test 15 --n-test 5 --n-train 5 --k-train 20 --augment --size large
+python experiments/proto_nets.py --dataset fashion --k-test 2 --n-test 5 --n-train 1 --k-train 20 --augment --size large
+python experiments/proto_nets.py --dataset fashion --k-test 5 --n-test 5 --n-train 1 --k-train 20 --augment --size large
+python experiments/proto_nets.py --dataset fashion --k-test 15 --n-test 5 --n-train 1 --k-train 20 --augment --size large
 
+# STNs (coeff 9 dropout 0.5)
+python experiments/proto_nets.py --dataset fashion --k-test 2 --n-test 1 --n-train 1 --k-train 20 --constrained --stn_reg_coeff 9.0 --stn 1 --size large
+python experiments/proto_nets.py --dataset fashion --k-test 5 --n-test 1 --n-train 1 --k-train 20 --constrained --stn_reg_coeff 9.0 --stn 1 --size large
+python experiments/proto_nets.py --dataset fashion --k-test 15 --n-test 1 --n-train 1 --k-train 20 --constrained --stn_reg_coeff 9.0 --stn 1 --size large
+python experiments/proto_nets.py --dataset fashion --k-test 2 --n-test 5 --n-train 1 --k-train 20 --constrained --stn_reg_coeff 9.0 --stn 1 --size large
+python experiments/proto_nets.py --dataset fashion --k-test 5 --n-test 5 --n-train 1 --k-train 20 --constrained --stn_reg_coeff 9.0 --stn 1 --size large
+python experiments/proto_nets.py --dataset fashion --k-test 15 --n-test 5 --n-train 1 --k-train 20 --constrained --stn_reg_coeff 9.0 --stn 1 --size large
